768c833ed7ce7d1350c6884057ee21924527a191
Pradeep9550 <kapil95550@gmail.com>
2025-06-12 01:58:58 +0530

    a

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import MessageContainer from '../../components/messages/MessageContainer'
 
 const Home = () => {
   return (
-    <div className='flex h-[650px] sm:h-[450px] md:h-[550px] lg:h-[550px] rounded-lg overflow-auto bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
+    <div className='flex h-[610px] sm:h-[450px] md:h-[550px] lg:h-[550px] rounded-lg overflow-auto bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
      <Sidebar/>
      <MessageContainer/>
     </div>
